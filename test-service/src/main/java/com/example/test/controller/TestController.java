1869fd7e1b9d9879c3f579a2ffb49b12cfe8d992
kishore madugula <kishore.madugula@outlook.com>
2022-01-23 21:07:18 -0500

    lombok added

--- test-service/src/main/java/com/example/test/controller/TestController.java
+++ test-service/src/main/java/com/example/test/controller/TestController.java
@@ -1,15 +1,17 @@
 package com.example.test.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController("/test")
+@Slf4j
 public class TestController {
     @GetMapping
     public String testService(HttpServletRequest request) {
-        System.out.println("I am " + request.getRequestURL().toString());
+        log.info("I am " + request.getRequestURL().toString());
         return request.getRequestURL().toString();
     }
 

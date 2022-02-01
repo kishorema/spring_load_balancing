2e72d37fdf39bbf9463833a6428da651b3ee8550
kishore madugula <kishore.madugula@outlook.com>
2022-01-31 19:14:16 -0500

    added logic for Book for both XML & json response types in same method

--- test-service/src/main/java/com/example/test/controller/TestController.java
+++ test-service/src/main/java/com/example/test/controller/TestController.java
@@ -1,18 +1,30 @@
 package com.example.test.controller;
 
+import com.example.test.Book;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-@RestController("/test")
+@RestController
+@RequestMapping("users")
 @Slf4j
 public class TestController {
-    @GetMapping
+    @GetMapping(path="/ping")
     public String testService(HttpServletRequest request) {
         log.info("I am " + request.getRequestURL().toString());
         return request.getRequestURL().toString();
     }
 
+    @GetMapping(path = "/book/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public Book getBook(@PathVariable int id) {
+        Book book = new Book();
+        book.setId(id);
+        book.setTitle("Spring 5 Guide");
+        return book;
+    }
 }
